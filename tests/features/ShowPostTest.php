104a45b544df5ca5d6313ed3e77286b24ed3dd45
Ismael Haytam <anonismaa@gmail.com>
2017-04-27 21:43:48 +0200

    TDD Pruebas de regresión - URL dinámica de un post

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -17,19 +17,41 @@ class ShowPostTest extends TestCase
         ]);
 
         $post = factory(\App\Post::class)->make([
-            'title' => 'Como instalar Laravel',
-            'title' => 'Esste el el contenido del post'
+            'title' => 'Este es el titulo del post',
+            'content' => 'Este es el contenido del post'
         ]);
 
         $user->posts()->save($post);
 
         // when
 
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
              ->seeInElement('h1', $post->title)
              ->see($post->content)
              ->see($user->name);
 
     }
+
+    function test_old_urls_are_redirected()
+    {
+
+        // having
+
+        $user = $this->defaultUser();
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title'
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)->seePageIs($post->url);
+
+    }
+
 
 }
