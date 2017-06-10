b34c46bbe07aeca34fdb869d266be7acfeb67c1b
Ismael Haytam <anonismaa@gmail.com>
2017-06-10 05:54:59 +0200

    Refactorizar la prueba añadiendo el método setUp TDD

--- tests/integration/APostCanBeVotedTest.php
+++ tests/integration/APostCanBeVotedTest.php
@@ -8,96 +8,84 @@ class APostCanBeVotedTest extends TestCase
 
     use DatabaseTransactions;
 
+    protected $user;
+    protected $post;
+
+    function setUp()
+    {
+        parent::setUp();
+
+        $this->actingAs($this->user = $this->defaultUser());
+
+        $this->post = $this->createPost();
+    }
+
     function test_a_post_can_be_upvoted()
     {
-        $this->actingAs($user = $this->defaultUser());
-
-        $post = $this->createPost();
-
-        Vote::upvote($post);
+        Vote::upvote($this->post);
 
         $this->assertDatabaseHas('votes', [
-            'post_id' => $post->id,
-            'user_id' => $user->id,
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
             'vote' => 1
         ]);
 
-        $this->assertSame(1, $post->score);
+        $this->assertSame(1, $this->post->score);
     }
 
     function test_a_post_can_be_downvoted()
     {
-        $this->actingAs($user = $this->defaultUser());
-
-        $post = $this->createPost();
-
-        Vote::downvote($post);
+        Vote::downvote($this->post);
 
         $this->assertDatabaseHas('votes', [
-            'post_id' => $post->id,
-            'user_id' => $user->id,
+            'post_id' => $this->post->id,
+            'user_id' => $this->user->id,
             'vote' => -1
         ]);
 
-        $this->assertSame(-1, $post->score);
+        $this->assertSame(-1, $this->post->score);
     }
 
     function test_a_post_cannot_be_upvoted_twice_by_the_same_user()
     {
-        $this->actingAs($user = $this->defaultUser());
-
-        $post = $this->createPost();
-
-        Vote::upvote($post);
-        Vote::upvote($post);
+        Vote::upvote($this->post);
+        Vote::upvote($this->post);
 
         $this->assertSame(1, Vote::count());
 
-        $this->assertSame(1, $post->score);
+        $this->assertSame(1, $this->post->score);
     }
 
     function test_a_post_cannot_be_downvoted_twice_by_the_same_user()
     {
-        $this->actingAs($user = $this->defaultUser());
-
-        $post = $this->createPost();
-
-        Vote::downvote($post);
-        Vote::downvote($post);
+        Vote::downvote($this->post);
+        Vote::downvote($this->post);
 
         $this->assertSame(1, Vote::count());
 
-        $this->assertSame(-1, $post->score);
+        $this->assertSame(-1, $this->post->score);
     }
 
     function test_a_user_can_switch_from_upvote_to_downvote()
     {
-        $this->actingAs($user = $this->defaultUser());
+        Vote::upvote($this->post);
 
-        $post = $this->createPost();
-
-        Vote::upvote($post);
-
-        Vote::downvote($post);
+        Vote::downvote($this->post);
 
         $this->assertSame(1, Vote::count());
 
-        $this->assertSame(-1, $post->score);
+        $this->assertSame(-1, $this->post->score);
     }
 
     function test_a_user_can_switch_from_downvote_to_upvote()
     {
-        $this->actingAs($user = $this->defaultUser());
+        Vote::downvote($this->post);
 
-        $post = $this->createPost();
-
-        Vote::downvote($post);
-
-        Vote::upvote($post);
+        Vote::upvote($this->post);
 
         $this->assertSame(1, Vote::count());
 
-        $this->assertSame(1, $post->score);
+        $this->assertSame(1, $this->post->score);
     }
 
 }
