98f22a8e2f5ac8d913c0b1b428fdfe6aa29ab3c9
Ismael Haytam <anonismaa@gmail.com>
2017-06-16 00:34:40 +0200

    API para votar por un post

--- tests/features/VoteForPostTest.php
+++ tests/features/VoteForPostTest.php
@@ -79,4 +79,25 @@ class VoteForPostTest extends TestCase
          $this->assertSame(0, $post->fresh()->score);
     }
 
+    function test_a_guest_user_cannot_vote_for_a_post()
+    {
+        $user = $this->defaultUser();
+
+        $post = $this->createPost();
+
+        $this->postJson("{$post->url}/upvote")
+             ->assertStatus(401)
+             ->assertJson(['error' => 'Unauthenticated.']);
+
+        $this->assertDatabaseMissing('votes', [
+            'post_id' => $post->id,
+            'user_id' => $user->id
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+           'id' => $post->id,
+           'score' => 0
+        ]);
+    }
+
 }
