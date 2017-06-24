4ef5e6b4a2a078e81917fa1968c105da529e2256
Ismael Haytam <anonismaa@gmail.com>
2017-06-24 14:53:30 +0200

    Creación de un feature con Laravel y Vue.js

---
+++ resources/assets/js/components/Vote.vue
@@ -0,0 +1,20 @@
+
+<template>
+
+    <div>
+        <form>
+            <button type="button" class="btn btn-default">+1</button>
+            Puntuacón actual: <strong id="current-score">5</strong>
+            <button type="button" class="btn btn-default">-1</button>
+        </form>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
