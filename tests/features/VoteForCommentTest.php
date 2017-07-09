7426112e1c3684908f130e2c6a2b1b01f8ca1685
Ismael Haytam <anonismaa@gmail.com>
2017-07-09 22:35:50 +0200

    Votar por un comentario - Relaciones polimórficas - TDD

--- tests/features/VoteForCommentTest.php
+++ tests/features/VoteForCommentTest.php
@@ -6,12 +6,23 @@ class VoteForCommentTest extends TestCase
     use DatabaseTransactions;
     protected $comment;
     protected $user;
-
     public function setUp()
     {
         parent::setUp();
         $this->actingAs($this->user = $this->defaultUser());
         $this->comment = factory(Comment::class)->create();
     }
-    
+    function test_a_user_can_upvote_for_a_comment()
+    {
+        $this->postJson("comments/{$this->comment->id}/vote/1")
+            ->assertSuccessful()
+            ->assertJson([
+                'new_score' => 1
+            ]);
+        $this->comment->refresh();
+        $this->assertSame(1, $this->comment->current_vote);
+        $this->assertSame(1, $this->comment->score);
+    }
+
+
 }
