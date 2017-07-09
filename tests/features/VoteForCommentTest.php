c52ec1b7f43b60b0ff1aca4f5b6b0a4e9e3e5ff2
Ismael Haytam <anonismaa@gmail.com>
2017-07-09 22:37:58 +0200

    Votar por un comentario - Relaciones polimórficas - TDD

--- tests/features/VoteForCommentTest.php
+++ tests/features/VoteForCommentTest.php
@@ -1,17 +1,22 @@
 <?php
+
 use App\Comment;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 class VoteForCommentTest extends TestCase
 {
+
     use DatabaseTransactions;
     protected $comment;
     protected $user;
+
     public function setUp()
     {
         parent::setUp();
         $this->actingAs($this->user = $this->defaultUser());
         $this->comment = factory(Comment::class)->create();
     }
+
     function test_a_user_can_upvote_for_a_comment()
     {
         $this->postJson("comments/{$this->comment->id}/vote/1")
@@ -23,6 +28,7 @@ class VoteForCommentTest extends TestCase
         $this->assertSame(1, $this->comment->current_vote);
         $this->assertSame(1, $this->comment->score);
     }
+
     function test_a_user_can_downvote_for_a_comment()
     {
         $this->postJson("comments/{$this->comment->id}/vote/-1")
@@ -34,6 +40,7 @@ class VoteForCommentTest extends TestCase
         $this->assertSame(-1, $this->comment->current_vote);
         $this->assertSame(-1, $this->comment->score);
     }
+
     function test_a_user_can_unvote_a_comment()
     {
         $this->comment->upvote();
@@ -46,6 +53,7 @@ class VoteForCommentTest extends TestCase
         $this->assertNull($this->comment->current_vote);
         $this->assertSame(0, $this->comment->score);
     }
+
     function test_a_guest_user_cannot_vote_for_a_comment()
     {
         auth()->logout();
@@ -56,4 +64,5 @@ class VoteForCommentTest extends TestCase
         $this->assertNull($this->comment->current_vote);
         $this->assertSame(0, $this->comment->score);
     }
+
 }
